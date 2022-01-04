678070d52127c8fc1507297f86c8e0f199fc1d16
Georg Sieber <it@georg-sieber.de>
2022-01-04 20:19:37 +0100

    remove unused imports

--- app/src/main/java/systems/sieber/remotespotlight/ControlActivity.java
+++ app/src/main/java/systems/sieber/remotespotlight/ControlActivity.java
@@ -23,8 +23,6 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
